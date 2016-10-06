99041ecd1fe514d5c456f73edd69c0cf00a19a72
Andrew <andrew.thielcole@gmail.com>
2016-10-05 20:57:04 -0700

    Default Thumbnail for images

--- src/model/movement.ts
+++ src/model/movement.ts
@@ -17,7 +17,7 @@ export class Movement {
 		this.id = obj && obj.id || '';
 		this.verified = obj && obj.verified || false;
 		this.videoUrl = obj && obj.videoUrl || '';
-		this.thumbnail = obj && obj.thumbnail || '';
+		this.thumbnail = obj && obj.thumbnail || { 'imageURL': 'https://firebasestorage.googleapis.com/v0/b/popping-inferno-7577.appspot.com/o/images%2Fdumbell%20logo.png?alt=media&token=c985e1bc-4253-42b8-ad5c-4bcda93385a2', name:'defaultDumbel' };
 		this.images = obj && obj.images || [];
 	}
 
