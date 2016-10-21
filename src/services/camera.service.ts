cce5a8f6a6cbd926ca88e9428d86bb5f5e3cd2ce
Andrew <andrew.thielcole@gmail.com>
2016-10-21 09:34:10 -0700

    Crop android photos, refactor camera service

--- src/services/camera.service.ts
+++ src/services/camera.service.ts
@@ -7,12 +7,14 @@ import { AuthService } from './auth.service';
 declare var window: any;
 declare var Zone: any;
 declare var VideoEditor: any;
+declare var plugins: any;
 
 @Injectable()
 export class CameraService {
 
     // Ability to upload videos
   public options: any = {
+        allowEdit: true,
         sourceType: Camera.PictureSourceType.SAVEDPHOTOALBUM,
         mediaType: Camera.MediaType.ALLMEDIA,
         destinationType: Camera.DestinationType.FILE_URI
@@ -43,6 +45,7 @@ export class CameraService {
         }
 
         // find file type video or image
+
         const fileType = this._findFileType(fileUri);
 
         if (fileType.substring( 0, 5) === 'video') {
@@ -50,10 +53,23 @@ export class CameraService {
             console.log('Error Caught while creating thumbnail: ' + JSON.stringify(error));
             reject(error);
           }).then( (thumbnail) => {
-            this.resolveLocalFile(resolve, reject, fileUri, thumbnail);
+            this._resolveLocalFile(resolve, reject, fileUri, thumbnail);
           });
         } else {
-          this.resolveLocalFile(resolve, reject, fileUri, null);
+          // Images for android are cropped with seperate plugin
+          if (this.platform.is('android')) {
+            let options = {};
+            plugins.crop.promise( fileUri, options).then( (path) => {
+              console.log('Cropped Image Path: ' + JSON.stringify(path));
+              this._resolveLocalFile(resolve, reject, path, null);
+            }).catch((error) => {
+              console.log('Error Cropping Android: ' + JSON.stringify(error));
+              reject(error);
+            });
+          } else {
+            // iOS Cropping handled natively with allowEdit option
+            this._resolveLocalFile(resolve, reject, fileUri, null);
+          }
         }
       }).catch( (error) => {
         console.log('Get Picture Error: ' , error);
@@ -73,7 +89,7 @@ export class CameraService {
             fileUri = 'file://' + fileUri;
           }
 
-          this.resolveLocalFile( resolve, reject, fileUri, null );
+          this._resolveLocalFile( resolve, reject, fileUri, null );
         }, (error) => {
           console.log('Thumbnail Error: ' + JSON.stringify(error));
           reject(error);
@@ -85,12 +101,12 @@ export class CameraService {
     });
   }
 
-  resolveLocalFile(resolve, reject, fileUri, thumbnail) {
+  _resolveLocalFile(resolve, reject, fileUri, thumbnail) {
      window.resolveLocalFileSystemURL(fileUri, (fileEntry) => {
          fileEntry.file( (file) => {
              // Create Thumbnail
              console.log('File: ' + (typeof file) + ', ' + JSON.stringify(file));
-             this.readFile( resolve, reject, file, thumbnail);
+             this._readFile( resolve, reject, file, thumbnail);
            }, (error) => {
              console.log('File Entry Error: ' + JSON.stringify(error));
              reject(error);
@@ -102,7 +118,7 @@ export class CameraService {
 
   }
 
-  readFile(resolve, reject, file, thumbnail) {
+  _readFile(resolve, reject, file, thumbnail) {
     const fileReader = new FileReader();
     // iOS giving null for file type
     if (file.type === null) {
@@ -114,7 +130,7 @@ export class CameraService {
      // Create Thumbnail
      let arrayBuffer = result.target.result;
      let blob = new Blob([new Uint8Array(arrayBuffer)], {'type': '' + file.type});
-     this.upload(blob, file.name , file.type, thumbnail, resolve, reject);
+     this.storage.upload(blob, file.name , file.type, thumbnail, resolve, reject);
    };
 
    fileReader.onloadstart = () => {
@@ -132,59 +148,6 @@ export class CameraService {
 
    fileReader.readAsArrayBuffer(file);
   }  
-
-  upload(blob: Blob, name: string, type: string, thumbnail: any, resolve: any, reject: any) {
-    console.log('Uploading: ' + JSON.stringify(name) + ' , type: ' + JSON.stringify(type));
-    let uploadTask = this.storage.addBlob( name , type, blob);
-   uploadTask.on('state_changed' , (snapshot) => {
-      // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
-      var progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-      console.log('Upload is ' + progress + '% done');
-      switch (snapshot.state) {
-        case firebase.storage.TaskState.PAUSED: // or 'paused'
-          console.log('Upload is paused');
-          break;
-        case firebase.storage.TaskState.RUNNING: // or 'running'
-          console.log('Upload is running');
-          break;
-      }
-    }, (error: any) => {
-      console.log('An Error has occured');
-      switch (error.code) {
-        case 'storage/unauthorized':
-          // User doesn't have permission to access the object
-          console.log('Unauthorized Access: ' + JSON.stringify(error));
-          reject(error.code);
-          break;
-        case 'storage/canceled':
-          // User canceled the upload
-          console.log('Canceled');
-          reject(error.code);
-          break;
-        case 'storage/unknown':
-          // Unknown error occurred, inspect error.serverResponse
-          console.log('Storage Unkown: ' + JSON.stringify(error));
-          reject(error.code);
-          break;
-        default:
-          console.log('Default: ' + JSON.stringify(error));
-          reject(error.code);
-          break;
-      }
-    }, () => {
-      const uploadReturn = {
-        url: uploadTask.snapshot.downloadURL,
-        name: uploadTask.snapshot.metadata.name,
-        mediaType: type
-      };
-      if (thumbnail) {
-        // Only add thumbnail for videos
-        uploadReturn['thumbnail'] = thumbnail;
-      };
-      console.log('Finished Uploading: ' + JSON.stringify(uploadReturn));
-      resolve(uploadReturn);
-    });
-  }
 
   _findFileType(fileName: string): string {
     // Find the suffix (ex .mp4) and translate
--- src/services/storage.service.ts
+++ src/services/storage.service.ts
@@ -22,7 +22,7 @@ export class StorageService {
       case 'video/mov': 
         return this.addVideo(name, blob);
       default:
-        console.log('No Type specified in blob addition');
+        console.log('Type Not Supported');
         break;
     }
 	}
@@ -37,5 +37,58 @@ export class StorageService {
 
   addFile(name, file) {
     return this.imageRef.child(name).put(file);
+  }
+
+  upload(blob: Blob, name: string, type: string, thumbnail: any, resolve: any, reject: any) {
+   console.log('Uploading: ' + JSON.stringify(name) + ' , type: ' + JSON.stringify(type));
+   let uploadTask = this.addBlob( name , type, blob);
+   uploadTask.on('state_changed' , (snapshot) => {
+      // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+      var progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+      console.log('Upload is ' + progress + '% done');
+      switch (snapshot.state) {
+        case firebase.storage.TaskState.PAUSED: // or 'paused'
+          console.log('Upload is paused');
+          break;
+        case firebase.storage.TaskState.RUNNING: // or 'running'
+          console.log('Upload is running');
+          break;
+      }
+    }, (error: any) => {
+      console.log('An Error has occured');
+      switch (error.code) {
+        case 'storage/unauthorized':
+          // User doesn't have permission to access the object
+          console.log('Unauthorized Access: ' + JSON.stringify(error));
+          reject(error.code);
+          break;
+        case 'storage/canceled':
+          // User canceled the upload
+          console.log('Canceled');
+          reject(error.code);
+          break;
+        case 'storage/unknown':
+          // Unknown error occurred, inspect error.serverResponse
+          console.log('Storage Unkown: ' + JSON.stringify(error));
+          reject(error.code);
+          break;
+        default:
+          console.log('Default: ' + JSON.stringify(error));
+          reject(error.code);
+          break;
+      }
+    }, () => {
+      const uploadReturn = {
+        url: uploadTask.snapshot.downloadURL,
+        name: uploadTask.snapshot.metadata.name,
+        mediaType: type
+      };
+      if (thumbnail) {
+        // Only add thumbnail for videos
+        uploadReturn['thumbnail'] = thumbnail;
+      };
+      console.log('Finished Uploading: ' + JSON.stringify(uploadReturn));
+      resolve(uploadReturn);
+    });
   }
 }
